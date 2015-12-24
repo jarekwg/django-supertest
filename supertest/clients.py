4a5e14c5c277800ffe1d8b9a1d7c116bfac98604
Luke Hodkinson <furious-luke@gmail.com>
2015-12-25 00:34:19 +1100

    Adding JSON data and responses to the AJAX client.

--- supertest/clients.py
+++ supertest/clients.py
@@ -5,16 +5,26 @@ from django.test import Client
 
 class AjaxClient(Client):
 
-    def _get_kwargs(self, kwargs):
+    def _get_kwargs(self, kwargs, extra={}):
         kw = {'HTTP_X_REQUESTED_WITH': 'XMLHttpRequest'}
         kw.update(kwargs)
+        kw.update(extra)
         return kw
 
-    def post(self, *args, **kwargs):
-        return super(AjaxClient, self).post(*args, **self._get_kwargs(kwargs))
+    def _handle_json(self, data, kwargs):
+        extra = {}
+        if kwargs.pop('json', False):
+            data = json.dumps(data)
+            extra['content_type'] = 'application/json'
+        return data, extra
 
-    def get(self, *args, **kwargs):
-        return super(AjaxClient, self).get(*args, **self._get_kwargs(kwargs))
+    def post(self, url, data=None, **kwargs):
+        data, extra = self._handle_json(data, kwargs)
+        return super(AjaxClient, self).post(url, data, **self._get_kwargs(kwargs, extra))
+
+    def get(self, url, data=None, **kwargs):
+        data, extra = self._handle_json(data, kwargs)
+        return super(AjaxClient, self).get(url, data, **self._get_kwargs(kwargs))
 
     def content(self, response):
         return json.loads(response.content.decode('utf-8'))
